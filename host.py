5604d9cc2fca45e3c0def51dd8665ce792ad57ed
Om Gupta <om.gupta.1507@gmail.com>
2020-11-20 18:28:06 +0530

    Update host.py

--- host.py
+++ host.py
@@ -1,8 +1,6 @@
-import flask, base64, json, os, math, threading, random, waitress, qrcode, mysql.connector
+import flask, base64, json, os, math, threading, random, waitress, qrcode, mysql.connector, flask_limiter
 from AES import AES
-from flask_limiter import Limiter
-
-import config
+from flask_limiter.util import get_remote_address
 
 os.system("title CodesBin Host")
 
@@ -15,8 +13,38 @@ def getDatabase():
     )
 
 app = flask.Flask(__name__)
+limiter = flask_limiter.Limiter(app, key_func = get_remote_address)
+
+project = {
+    "name": "CodesBin",
+    "website": "https://codesbin.my.to",
+    "host": "codesbin.my.to"
+}
 
 ####################################################################################
+
+# CREATING THE POSTS TABLE:
+
+with getDatabase() as database:
+    with database.cursor() as cursor:
+        cursor.execute("""
+            CREATE TABLE Posts (
+                id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                created BIGINT,
+                urlId LONGTEXT,
+                enabled BOOLEAN,
+                expired BOOLEAN,
+                doesExpire BOOLEAN,
+                expiryClicks BIGINT,
+                expiryTime BIGINT,
+                views BIGINT,
+                passwordProtected BOOLEAN,
+                data LONGTEXT,
+                qrCodeUrl LONGTEXT,
+                lastView BIGINT
+            )
+        """)
+        database.commit()
 
 ####################################################################################
 
